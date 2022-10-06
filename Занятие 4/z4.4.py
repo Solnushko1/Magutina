ac734a800e3bd63916c003e8fc9bf42d4f0abd76
Solnushko1 <magutina.vita@mail.ru>
2022-10-06 22:38:24 +0300

    Занятие 4

---
+++ Занятие 4/z4.4.py
@@ -0,0 +1,7 @@
+def summa(n):
+    sum = 0
+    for i in range(n):
+        znach = int(input())
+        sum += znach
+    print(sum)
+summa(int(input()))
---
+++ Занятие 4/z4.5.py
@@ -0,0 +1,7 @@
+def stepen(n):
+    sum = 0
+    for i in range(1, n+1):
+        znach = i**3
+        sum += znach
+    print(sum)
+stepen(int(input()))
