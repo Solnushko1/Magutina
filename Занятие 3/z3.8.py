c4d57d2ff3c3d4fdf6366de0d79223cb68ac03c2
Solnushko1 <magutina.vita@mail.ru>
2022-10-14 13:53:03 +0300

    Занятие 3

--- Занятие 3/z3.8.py
+++ Занятие 3/z3.8.py
@@ -1,10 +1,10 @@
-def compare(a, b, c):
+def numbers(a,b,c):
     if a == b and a == c and b == c:
         print('3')
-    elif a == b or a == c or b == c:
+    if a == b or a == c or b == c:
         print('2')
     else:
         print('0')
 
 
-compare(int(input()), int(input()), int(input()))
+numbers(int(input()), int(input()), int(input()))
