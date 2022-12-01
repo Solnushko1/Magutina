02df11333570f8d671e67da910b1d6466aa479cc
Solnushko1 <magutina.vita@mail.ru>
2022-12-01 09:16:00 +0300

    Занятие 10

--- Занятие 10/z10.2.py
+++ Занятие 10/z10.2.py
@@ -1,29 +1,22 @@
+matrix = [[7,22,17],[9,30,27],[13,14,12]]
+def yupi(x):
 
-def matrix_izmenineie(m):
-        for row in range (0,len(m)):
+    list = []
+    count = 0
+    for row in range(0, len(matrix)):
+        for i in range(3):
             string = ''
-            if row % 2 != 0:
-                for t in range (0,len(m[row])):
-                    string = string + "1" + '    '
-                print(string)
-            else:
-                for t in range (0,len(m[row])):
-                    string = string + "0" + '    '
-                print(string)
-
-def yupi_dly_otkrivaniy(f):
-    array = []
-    while True:
-        x = f.readline().strip()
-        if (x == ""):
-            break
-        array.append(str(x))
-    return array
-
-yupi_dly_chteniy = open("C:\\Users\\My\\PycharmProjects\\pythonProject\\Основы\\lesson 10\\ex2.txt", "r")
-matrix = yupi_dly_otkrivaniy(yupi_dly_chteniy)
-print("Начальная матрица:", matrix)
-
-
-matrix = [[1,5,3],[8,2,4],[9,11,7]]
-matrix_izmenineie(matrix)
+            if matrix[row][i] % 2 != 0:
+                string += "1"
+                list.append(string)
+            elif matrix[row][i] % 2 == 0:
+                string += "0"
+                list.append(string)
+    with open('matrix', 'w') as file:
+        #file.write(str(list))
+        for k in range(3):
+            for i in range(3):
+                file.write(list[count] + ' ')
+                count += 1
+            file.write('\n')
+yupi(matrix)
